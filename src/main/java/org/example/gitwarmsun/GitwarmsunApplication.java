4cbfde650c46313b9b7bc926268112184034a258
Wangbangc <747880962@qq.com>
2025-02-25 22:57:56 +0800

    first commit

---
+++ src/main/java/org/example/gitwarmsun/GitwarmsunApplication.java
@@ -0,0 +1,13 @@
+package org.example.gitwarmsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitwarmsunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitwarmsunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/gitwarmsun/GitwarmsunApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitwarmsun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitwarmsunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
